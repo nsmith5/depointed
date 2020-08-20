f4cafbee42c80e0012a658fadddff9a17ea59ef9
Nathan Smith <nathan@nfsmith.ca>
2020-08-20 16:56:58 -0700

    Add a simple drawing app frontend

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "https://strokeless.nfsmith.ca"
   :license {:name "GPL 3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [compojure "1.6.1"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]]
   :main ^:skip-aot strokeless.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/strokeless/core.clj
+++ src/strokeless/core.clj
@@ -1,7 +1,19 @@
 (ns strokeless.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.util.response :refer [resource-response]]
+            [compojure.core :refer [defroutes GET]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defroutes routes
+  (GET "/" [] (resource-response "public/index.html")))
+
+(def app
+  (-> routes
+      (wrap-resource "public")))
+
+(defn server [] (run-jetty app {:join? false, :port 3000}))
+
+(defn -main [& args]
+  (server))
