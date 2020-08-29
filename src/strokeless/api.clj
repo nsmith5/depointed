c6b71e043fddc2a79bc4860bcb782120560e9161
Nathan Smith <nathan@nfsmith.ca>
2020-08-29 15:37:59 -0700

    Refactor server code

---
+++ src/strokeless/api.clj
@@ -0,0 +1,45 @@
+(ns strokeless.api
+    (:require [clojure.data.json :as json]
+              [ring.middleware.params :refer [wrap-params]]
+              [ring.util.response :as response]
+              [ring.util.request :as request]
+              [strokeless.storage :as storage]))
+
+(defn upload 
+    "Handle upload of new labelled imaged" 
+    [req]
+    (let [label (get (:query-params req) "label")
+          image (.bytes (:body req))]
+         (storage/store-labelled-image label image))
+    (-> {"msg" "Image accpeted"}
+        (json/write-str)
+        (response/response)
+        (response/status 201)
+        (response/content-type "application/json")))
+
+(defn train 
+    "Update model by training against uploaded images" 
+    [req]
+    (-> {:error "Model training not implimented yet"}
+        (json/write-str)
+        (response/response)
+        (response/status 501)
+        (response/content-type "application/json")))
+
+(defn predict
+    "Predict character from image using model" 
+    [req]
+    (-> {:error "Model training not implimented yet"}
+        (json/write-str)
+        (response/response)
+        (response/status 501)
+        (response/content-type "application/json")))
+
+(defn handler
+    [req]
+    (let [path (request/path-info req)]
+        (cond
+            (= "/api/upload" path) (upload req)
+            (= "/api/train" path) (train req)
+            (= "/api/predict" path) (predict req)
+            :else (response/not-found "Endpoint not found"))))
---
+++ src/strokeless/server.clj
@@ -0,0 +1,24 @@
+(ns strokeless.server
+  (:require [clojure.string :as string] 
+            [org.httpkit.server :refer [run-server]]
+            [ring.util.request :as request]
+            [strokeless.spa :as spa]
+            [strokeless.api :as api]))
+
+; Default ring handler
+(defn default-handler
+    "Mux requests between the SPA and API handlers"
+    [req]
+    (let [path (request/path-info req)]
+        (if (string/starts-with? path "/api")
+            (api/handler req)
+            (spa/handler req))))
+
+; Default server configuration
+(def default-config {:port 3000})
+
+(defn run
+    "Run httpkit server"
+    ([] (run default-handler))
+    ([handler] (run handler default-config))
+    ([handler config] (run-server handler config)))
