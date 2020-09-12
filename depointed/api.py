d62072e9833963bef868c079fbbdd9a9ba74a28e
Nathan Smith <nathan@nfsmith.ca>
2020-09-11 22:32:36 -0700

    Implement image upload

--- depointed/api.py
+++ depointed/api.py
@@ -1,4 +1,9 @@
+import io
+import datetime
+import pathlib
+
 import flask
+import PIL.Image
 
 bp = flask.Blueprint('api', __name__, url_prefix='/api')
 
@@ -8,11 +13,23 @@ def upload():
     """upload accepts a labelled image and adds it to the data set for
     training.
     """
-    return flask.Response(
-        '{"msg": "Not Implemented"}',
-        status=501,
-        mimetype="application/json",
-    )
+    label = flask.request.args.get('label')
+    if label is None:
+        return {"error", "label query parameters required"}, 400
+    
+    directory = f'data/{label}/'
+    pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+    filepath = f'{directory}/{datetime.datetime.now().isoformat("T")}.png'
+
+    img = PIL.Image.open(io.BytesIO(flask.request.data)) 
+    img.thumbnail((32,32))
+    img.save(filepath) 
+   
+    response = {
+        'msg': 'image uploaded',
+        'path': filpath,
+    }
+    return response, 201
 
 
 @bp.route('/train', methods=['POST'])
