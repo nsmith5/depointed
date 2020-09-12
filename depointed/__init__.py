58d3f50ed7190d97a7acae4f795d11b4a67647ec
Nathan Smith <nathan@nfsmith.ca>
2020-09-11 20:18:31 -0700

    Simple flask server

--- depointed/__init__.py
+++ depointed/__init__.py
@@ -1,2 +1,8 @@
-def hello():
-    return "Hello!"
+import flask
+
+def create_app():
+    app = flask.Flask(__name__)
+    @app.route('/')
+    def hello():
+        return "Hello!"
+    return app
--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ from setuptools import setup
 setup(
     name="depointed",
     version="0.0.1",
+    description="depointed performs optical character recognition for single characters", 
     author="Nathan Smith",
     author_email="depointed@nfsmith.ca",
+    url="https://github.com/nsmith5/depointed",
     license="GPL3+",
-    packages=['depointed']
+    packages=['depointed'],
+    install_requires=[
+        'flask',
+    ],
+    scripts=['bin/depointed']
 )
